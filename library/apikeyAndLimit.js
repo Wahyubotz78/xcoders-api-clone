4192841fb82020a224976cb61bb0bed2b2726272
Wahyu BOTZ <93089824+Wahyubotz78@users.noreply.github.com>
2024-12-13 07:30:16 +0700

    Update apikeyAndLimit.js

--- library/apikeyAndLimit.js
+++ library/apikeyAndLimit.js
@@ -8,7 +8,7 @@ module.exports = async function (req, res, next) {
 	let apikey = req.query.apikey;
 	if (!apikey) return res.json({
 		status: false,
-		creator: "Farhannnnn",
+		creator: "Noth HDR",
 		message: "masukkan parameter apikey"
 	});
 	let users = await User.findOne({
